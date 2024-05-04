2f65d2c992efb730bd6d02081f7a2f2e97fb5b60
sk-2 <bboykensuke@gmail.com>
2024-05-04 19:44:41 +0900

    fixed layout

--- src/b-map/resources/js/Pages/Spots/Index.vue
+++ src/b-map/resources/js/Pages/Spots/Index.vue
@@ -172,63 +172,68 @@ const searchLocation = async () => {
 
     <Layout :success="success" v-if="currentPage === 'list'">
         <!-- リスト表示画面 -->
-        
-            <div class="flex flex-col">
-                <button @click="currentPage = 'map'" class="flex items-center w-32 h-9 bg-white ml-4 mb-7 border border-slate-300  rounded-lg">
-                    <img src="/images/pin_icon.png">
-                    <p>マップで表示</p>
+        <div>
+            <button @click="currentPage = 'map'" class="flex items-center h-9 bg-white pl-1 pr-4 mb-7 border border-slate-300  rounded-lg">
+                <img src="/images/pin_icon.png">
+                <p>マップで表示</p>
+            </button>
+            
+            <div class="flex h-12">
+                <label for="searchSpot" class="ml-4 pt-5">ワード検索</label>
+
+                <button @click="clearSearch" class="h-8 bg-white ml-auto mr-2 mb-7 px-2 border border-slate-300  rounded-lg">
+                    <p>検索をクリア</p>
                 </button>
-                
-                <div class="flex h-12">
-                    <label for="searchSpot" class="ml-4 pt-5">ワード検索</label>
-                    <button @click="clearSearch" class="w-24 h-8 bg-white ml-auto mr-2 mb-7 border border-slate-300  rounded-lg">
-                        <p>検索をクリア</p>
-                    </button>
-                </div>
-                
-                <div class="flex mb-4">
-                    <TextInput id="searchSpot" v-model="searchWord" type="text" class="ml-1 w-full" placeholder="駅・体育館"/>
-                    <button @click="searchSpot(searchURL)" class="flex justify-center items-center bg-gray-300 w-12 h-12">
-                        <img src="/images/search_spot_icon.png">
-                    </button>
-                </div>
-                                   
-                <!-- 通常時（全件表示） -->
-                <SpotCard v-if="!searchFlag" :spots="spots.data" class="content" />
-                <Paginate v-if="!searchFlag" :links="spots.links" />
-                <!-- 検索時 -->
-                <SpotCard v-if="searchFlag" :spots="responseSpots.data" class="content" />
-                <AsyncPaginate v-if="searchFlag" :links="responseSpots.links" @searchSpot="searchSpot"/>
-            </div>            
+            </div>
+            
+            <div class="flex mb-4">
+                <TextInput id="searchSpot" v-model="searchWord" type="text" class="w-full" placeholder="駅・体育館"/>
+
+                <button @click="searchSpot(searchURL)" class="flex justify-center items-center bg-gray-300 w-12 h-12">
+                    <img src="/images/search_spot_icon.png">
+                </button>
+            </div>
+                               
+            <!-- 通常時（全件表示） -->
+            <SpotCard v-if="!searchFlag" :spots="spots.data" class="content" />
+            <Paginate v-if="!searchFlag" :links="spots.links" />
+            <!-- 検索時 -->
+            <SpotCard v-if="searchFlag" :spots="responseSpots.data" class="content" />
+            <AsyncPaginate v-if="searchFlag" :links="responseSpots.links" @searchSpot="searchSpot"/>
+        </div>            
     </Layout>
 
     <!-- マップ表示画面 -->
     <div v-show="currentPage === 'map'" class="relative">
-            <div class="flex flex-col w-screen absolute z-10">
-                <div class="container mx-auto px-5 max-w-lg">
-                    <button @click="currentPage = 'list'" class="flex items-center w-32 h-9 bg-white ml-4 my-7 border border-slate-300  rounded-lg">
-                        <img src="/images/list_icon.png" class="px-2">
-                        <p>リストで表示</p>
+        <div class="flex flex-col w-screen absolute z-10">
+            <div class="container mx-auto px-5 max-w-lg">
+                <div class="flex items-center">
+                    <button @click="currentPage = 'list'" class="flex items-center h-9 bg-white pl-1 pr-4 my-7 border border-slate-300  rounded-lg">
+                    <img src="/images/list_icon.png" class="px-2">
+                    <p>リストで表示</p>
+                </button>
+
+                <button @click="getCurrentLocation" class="flex justify-center items-center h-9 bg-white ml-auto px-2 border border-slate-300 rounded-lg z-10">
+                    <img src="/images/target_icon.png" class="w-6 h-6 mr-1">
+                    <p>現在地</p>
+                </button>
+                </div>
+                
+
+                <div class="relative flex mb-4">
+                    <TextInput v-model="searchLocationWord" type="text" class="ml-1 w-full" placeholder="指定の住所・施設に移動"/>
+
+                    <button @click="searchLocation" class="flex justify-center items-center bg-gray-300 w-12 h-12">
+                        <img src="/images/search_spot_icon.png">
                     </button>
 
-                    <div class="relative flex mb-4">
-                        <TextInput v-model="searchLocationWord" type="text" class="ml-1 w-full" placeholder="指定の住所・施設に移動"/>
-                        <button @click="searchLocation" class="flex justify-center items-center bg-gray-300 w-12 h-12">
-                            <img src="/images/search_spot_icon.png">
-                        </button>
-
-                        <button @click="searchLocationWord = ''" class="absolute flex justify-center items-center top-3 right-14 bg-gray-300 w-5 h-5 rounded-full">
-                            <span class="text-sm">✖</span>
-                        </button>
-                    </div>
+                    <button @click="searchLocationWord = ''" class="absolute flex justify-center items-center top-3 right-14 bg-gray-300 w-5 h-5 rounded-full">
+                        <span class="text-sm">✖</span>
+                    </button>
                 </div>
             </div>
-
-            <div id="map" class="w-screen h-screen relative"></div>
-
-            <button @click="getCurrentLocation" class="absolute bottom-6 right-5 flex justify-center items-center w-24 h-9 bg-white border border-slate-300 rounded-lg z-10">
-                <img src="/images/target_icon.png" class="w-6 h-6">
-                <p class="ml-1">現在地</p>
-            </button>
         </div>
+
+        <div id="map" class="w-screen h-screen"></div>
+    </div>
 </template>
